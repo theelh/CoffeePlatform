daadd952e1e60458d85378e6a79f9f7e1526f7f0
theelh <theelhelh@gmail.com>
2024-11-25 12:47:30 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            backgroundImage: {
+                'login-background': "url('../images/imgBack1.jpg')",
+                'background2': "url('/img/footer-texture.png')",
+            }
+        },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            amber: colors.amber,
+            black: colors.black,
+            blue: colors.blue,
+            cyan: colors.cyan,
+            emerald: colors.emerald,
+            fuchsia: colors.fuchsia,
+            gray: colors.trueGray,
+            blueGray: colors.blueGray,
+            coolGray: colors.coolGray,
+            //trueGray: colors.trueGray,
+            warmGray: colors.warmGray,
+            green: colors.green,
+            indigo: colors.indigo,
+            lime: colors.lime,
+            orange: colors.orange,
+            pink: colors.pink,
+            purple: colors.purple,
+            red: colors.red,
+            rose: colors.rose,
+            sky: colors.sky,//warn - As of Tailwind CSS v2.2, `lightBlue` has been renamed to `sky`.
+            teal: colors.teal,
+            violet: colors.violet,
+            yellow: colors.amber,
+            white: colors.white,
+        },
+    },
+
+    plugins: [forms],
+};
